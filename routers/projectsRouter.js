598742af6dadf08cc8d6493423eb0f3a61a58c3c
chaycesol <chayce.solchaga@gmail.com>
2020-09-04 09:09:26 -0700

    all project routers completed

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 //import Project dataa
 const Projects = require('../data/helpers/projectModel');
+const Actions = require('../data/helpers/actionModel');
 
 // initializing router
 const router = express.Router()
 
 
 /* GET REQUESTS */
-
 // Returns List of Projects
 router.get('/', (req, res) => {
     Projects.get()
@@ -30,6 +30,17 @@ router.get('/:id', (req, res) => {
     })
 });
 
+// // Returns list of actions for Specific project by Project ID
+// router.get('/:id/actions', (req, res) => {
+//     Projects.getProjectActions(req.params.id)
+//     .then((actions) => {
+//         res.status(200).json(actions)
+//     })
+//     .catch((err) => {
+//         res.status(404).json({message: `actions for ${req.params.id} not found`})
+//     })
+// });
+
 /* POST REQUESTS */
 // Add a New Project
 router.post('/', (req, res) => {
@@ -40,6 +51,19 @@ router.post('/', (req, res) => {
     .catch((err) => {
         res.status(500).json({message: 'unable to submit data', error: {err}})
     })
+});
+
+//Add action to Project by ID
+router.post('/:id/actions', (req, res) => {
+   const newAction = {...req.body, id: Number(req.params.id)}
+
+   Actions.insert(req.body)
+   .then((newAction) =>{
+       res.status(200).json(newAction)
+   })
+   .catch((err) => {
+       res.status(500).json({ message: 'unable to add action to project'})
+   })
 });
 
 
