7dd4756125c09758a79aa743aedaa579f4fa0d7f
chaycesol <chayce.solchaga@gmail.com>
2020-09-04 09:40:25 -0700

    wrap up actions router migrated from projects

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -1,11 +1,70 @@
 const express = require('express');
 
-//import Project dataa
+// Preserve the req.params values from the parent router for nested router
+const router = express.Router({ mergeParams: true });
+
 const Actions = require('../data/helpers/actionModel');
+const Projects = require('../data/helpers/projectModel')
 
-// initializing router
-const router = express.Router()
+/* POST REQUESTS */
+// create action to specific project by ID
+router.post('/', (req, res) => {
+  const newPost = { ...req.body, project_id: req.params.id };
 
+  Actions.insert(newPost)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
+/* GET REQUESTS */
+// get all actions 
+router.get('/', (req, res) => {
+  Projects.getProjectActions(req.params.id)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// retrieve action by ID
+router.get('/:id', (req, res) => {
+  Actions.get(req.params.id)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+/* PUT REQUESTS */
+// adds an action by Project ID
+router.put('/:id', (req, res) => {
+  Actions.update(req.params.id, req.body)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+/* DELETE REQUESTS */
+// delete an action of a project by ID
+router.delete('/:id', (req, res) => {
+  Actions.remove(req.params.id)
+    .then((action) => {
+      res.status(200).json({ message: `records removed ${action}` });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
-//import Project dataa
-const Projects = require('../data/helpers/projectModel');
-const Actions = require('../data/helpers/actionModel');
+
 
 // initializing router
 const router = express.Router()
+
+// connecting actions route
+const actionsRouter = require('./actionsRouter');
+router.use('/:id/actions', actionsRouter);
+
+// import Project data
+const Projects = require('../data/helpers/projectModel');
 
 
 /* GET REQUESTS */
@@ -16,9 +21,10 @@ router.get('/', (req, res) => {
         res.status(200).json(projects)
     })
     .catch((err) => {
-        res.status(500).json({message: 'cannot retrieve projects,'})
-    })
+        res.status(500).json({message: 'unable to fetch project', error: {err}})
+    });
 });
+
 // Returns Specific Project with Actions by Project ID
 router.get('/:id', (req, res) => {
     Projects.get(req.params.id)
@@ -30,62 +36,47 @@ router.get('/:id', (req, res) => {
     })
 });
 
-// // Returns list of actions for Specific project by Project ID
-// router.get('/:id/actions', (req, res) => {
-//     Projects.getProjectActions(req.params.id)
-//     .then((actions) => {
-//         res.status(200).json(actions)
-//     })
-//     .catch((err) => {
-//         res.status(404).json({message: `actions for ${req.params.id} not found`})
-//     })
-// });
-
 /* POST REQUESTS */
 // Add a New Project
 router.post('/', (req, res) => {
-    Projects.insert(req.body)
-    .then((project) => {
-        res.status(200).json(project).end()
-    })
-    .catch((err) => {
-        res.status(500).json({message: 'unable to submit data', error: {err}})
-    })
-});
-
-//Add action to Project by ID
-router.post('/:id/actions', (req, res) => {
-   const newAction = {...req.body, id: Number(req.params.id)}
-
-   Actions.insert(req.body)
-   .then((newAction) =>{
-       res.status(200).json(newAction)
-   })
-   .catch((err) => {
-       res.status(500).json({ message: 'unable to add action to project'})
-   })
+    if (req.body.name && req.body.description) {
+        Projects.insert(req.body)
+        .then((project) => {
+            res.status(200).json(project).end()
+        })
+        .catch((err) => {
+            res.status(500).json({message: 'unable to submit data', error: {err}})
+        })
+    } else {
+        res.status(400).json({ message: 'missing required name or description field'})
+    }
 });
 
 
 /* PUT REQUESTS */
 router.put('/:id', (req, res) => {
-    Projects.update(req.params.id, req.body)
-    .then((updatedProject) => {
-        res.status(200).json(updatedProject)
-    })
-    .catch((err) => {
-        res.status(500).json({message: `unable to update or cant find a project with ID ${req.params.id}. please try again`})
-    })
+    if(req.body.name && req.body.description) {
+        Projects.update(req.params.id, req.body)
+        .then((updatedProject) => {
+            res.status(200).json(updatedProject)
+        })
+        .catch((err) => {
+            res.status(500).json({message: `unable to update or cant find a project with ID ${req.params.id}. please try again`})
+        })
+    } else {
+        res.status(401).json({ message: 'missing required field name or description'})
+    }
+    
 })
 
 /* DELETE REQUESTS */
 router.delete('/:id', (req, res) => {
     Projects.remove(req.params.id)
     .then((deleted) => {
-        res.status(201).json({message: `deleted project id: ${req.params.id}`})
+        res.status(201).json({message: `deleted project with id: ${req.params.id}`})
     })
     .catch((err) => {
-        res.status(500).json({error: 'error deleting project or cant find id. please try again.'})
+        res.status(404).json({error: 'error deleting project or cant find id. please try again.'})
     })
 })
 
